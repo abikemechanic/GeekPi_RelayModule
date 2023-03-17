a4f3316d9d906919ce1542bdce89a4eba6ebfbe2
abikemechanic <ariechman@protonmail.com>
2023-03-17 15:38:59 -0400

    adding more to the RPi relay module

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from GeekPI_relay_module import RelayBoard
+from mqtt.MQTTClient import MQTTClient
+
 import time
+import json
+
+
+def on_mqtt_message(client, userdata, message):
+    print(message.payload.decode('utf-8'))
+
+def load_configuration():
+    with open('relay_config.json') as json_config:
+        config = json.load(json_config)
+
+        for r in config['relays']:
+            print(f'relay id: {r["name"]}, topic: {r["subscribe_topic"]}')
 
 
 relayBoard = RelayBoard()
+mqttClient = MQTTClient(8888, '192.168.1.13')
+mqttClient.client.on_message = on_mqtt_message
 
 for r in relayBoard.relay_list:
     relayBoard.turn_on(r)
@@ -11,3 +27,6 @@ for r in relayBoard.relay_list:
 for r in relayBoard.relay_list:
     relayBoard.turn_off(r)
 
+
+if __name__ == '__main__':
+    load_configuration()
--- mqtt/MQTTClient.py
+++ mqtt/MQTTClient.py
@@ -2,12 +2,21 @@ import paho.mqtt.client as mqtt
 
 
 class MQTTClient:
-    def __init(self, port, hostname, subscribe_topics):
+    def __init__(self, port, hostname, subscribe_topics=[]):
         self.client = mqtt.Client(client_id='mqtt_control')
+        self.port = port
+        self.hostname = hostname
 
-        self.client.on_message = self.on_message
+        # self.client.on_message = self.on_message
 
     def on_message(self, client, userdata, message):
         print(f'message received, payload: {message.payload.decode("utf-8")},'
               f'message topic: {message.topic}')
+
+    def subscribe_to_topic(self, topic, qos=0):
+        self.client.subscribe(topic, qos=0)
+
+    def unsubscribe_from_topic(self, topic):
+        self.client.unsubscribe(topic)
+
 
